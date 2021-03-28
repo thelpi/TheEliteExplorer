654f94830028807ee30c07129371b965018ec365
Lpi <thelpi.v3@gmail.com>
2021-03-28 14:52:40 +0200

    Swagger

--- TheEliteExplorer/Startup.cs
+++ TheEliteExplorer/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Converters;
 using TheEliteExplorerCommon;
 using TheEliteExplorerDomain.Configuration;
 using TheEliteExplorerInfrastructure;
@@ -42,6 +43,9 @@ namespace TheEliteExplorer
         {
             services.AddMvc();
 
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
+          
             Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
 
             services
@@ -67,6 +71,17 @@ namespace TheEliteExplorer
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             ServiceProviderAccessor.SetProvider(app.ApplicationServices);
             app.UseMvc();
         }
