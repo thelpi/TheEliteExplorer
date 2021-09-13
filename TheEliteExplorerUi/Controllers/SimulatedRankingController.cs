8a8a9c39f8a126fd02eb0d628f119b0b6c08b129
Lpi <thelpi.v3@gmail.com>
2021-09-13 23:22:45 +0200

    clean code

--- TheEliteExplorerUi/Controllers/SimulatedRankingController.cs
+++ TheEliteExplorerUi/Controllers/SimulatedRankingController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TheEliteExplorerCommon;
+using TheEliteExplorerDomain;
 using TheEliteExplorerDomain.Abstractions;
 using TheEliteExplorerDomain.Enums;
 using TheEliteExplorerDomain.Models;
@@ -13,6 +14,10 @@ namespace TheEliteExplorerUi.Controllers
 {
     public class SimulatedRankingController : Controller
     {
+        private const int MaxRankDisplay = 50;
+        private const string StageImagePath = @"D:\Ma programmation\csharp\Projects\TheEliteUI\TheEliteUI\Resources\Stages\{0}.jpg";
+        private const string ViewName = "SimulatedRanking";
+
         private readonly IRankingProvider _rankingProvider;
 
         public SimulatedRankingController(IRankingProvider rankingProvider)
@@ -31,98 +36,35 @@ namespace TheEliteExplorerUi.Controllers
 
             var rankingEntries = rankingEntriesBase.Select(r => r as RankingEntry).ToList();
 
-            var pointsRankingEntries = new List<PointsRankingItemData>();
-            foreach (var entry in rankingEntries)
-            {
-                if (entry.Rank > 50) break;
-                pointsRankingEntries.Add(new PointsRankingItemData
-                {
-                    EasyPoints = entry.LevelPoints[Level.Easy],
-                    HardPoints = entry.LevelPoints[Level.Hard],
-                    MediumPoints = entry.LevelPoints[Level.Medium],
-                    PlayerColor = entry.PlayerColor,
-                    PlayerName = entry.PlayerName,
-                    Rank = entry.Rank,
-                    TotalPoints = entry.Points
-                });
-            }
+            var pointsRankingEntries = rankingEntries
+                .Where(r => r.Rank <= MaxRankDisplay)
+                .Select(r => r.ToPointsRankingItemData())
+                .ToList();
 
-            // TODO: manage equality between times (rare)
+            // this does not manage equality between two global times
+            // ie one player will be ranked above/below the other one
             int rank = 1;
-            var timeRankingEntries = new List<TimeRankingItemData>();
-            foreach (var entry in rankingEntries.OrderBy(x => x.CumuledTime))
+            var timeRankingEntries = rankingEntries
+                .OrderBy(x => x.CumuledTime)
+                .Take(MaxRankDisplay)
+                .Select(r => r.ToTimeRankingItemData( rank++))
+                .ToList();
+
+            var secondsLevel = SystemExtensions.Enumerate<Level>().ToDictionary(l => l, l => 0);
+            var stageWorldRecordEntries = game.GetStages()
+                .Select(s => s.ToStageWorldRecordItemData(rankingEntries, secondsLevel, StageImagePath))
+                .ToList();
+
+            return View($"Views/{ViewName}.cshtml", new SimulatedRankingViewData
             {
-                if (rank > 50) break;
-                timeRankingEntries.Add(new TimeRankingItemData
-                {
-                    EasyTime = new TimeSpan(0, 0, (int)entry.LevelCumuledTime[Level.Easy]),
-                    HardTime = new TimeSpan(0, 0, (int)entry.LevelCumuledTime[Level.Hard]),
-                    MediumTime = new TimeSpan(0, 0, (int)entry.LevelCumuledTime[Level.Medium]),
-                    PlayerColor = entry.PlayerColor,
-                    PlayerName = entry.PlayerName,
-                    Rank = rank,
-                    TotalTime = new TimeSpan(0, 0, (int)entry.CumuledTime)
-                });
-                rank++;
-            }
-
-            var easySeconds = 0;
-            var mediumSeconds = 0;
-            var hardSeconds = 0;
-            var stageWorldRecordEntries = new List<StageWorldRecordItemData>();
-            foreach (var stage in SystemExtensions.Enumerate<Stage>().Where(s => TheEliteExplorerDomain.Extensions.GetGame(s) == game))
-            {
-                var easyEntryRef = rankingEntries.Where(x => x.Details.ContainsKey(stage) && x.Details[stage].ContainsKey(Level.Easy)).OrderBy(x => x.Details[stage][Level.Easy].Item3).First();
-                var mediumEntryRef = rankingEntries.Where(x => x.Details.ContainsKey(stage) && x.Details[stage].ContainsKey(Level.Medium)).OrderBy(x => x.Details[stage][Level.Medium].Item3).First();
-                var hardEntryRef = rankingEntries.Where(x => x.Details.ContainsKey(stage) && x.Details[stage].ContainsKey(Level.Hard)).OrderBy(x => x.Details[stage][Level.Hard].Item3).First();
-
-                var easyTime = (int)easyEntryRef.Details[stage][Level.Easy].Item3;
-                var mediumTime = (int)mediumEntryRef.Details[stage][Level.Medium].Item3;
-                var hardTime = (int)hardEntryRef.Details[stage][Level.Hard].Item3;
-
-                var easyEntries = rankingEntries.Where(x => x.Details.ContainsKey(stage) && x.Details[stage].ContainsKey(Level.Easy) && x.Details[stage][Level.Easy].Item3 == easyTime).ToList();
-                var mediumEntries = rankingEntries.Where(x => x.Details.ContainsKey(stage) && x.Details[stage].ContainsKey(Level.Medium) && x.Details[stage][Level.Medium].Item3 == mediumTime).ToList();
-                var hardEntries = rankingEntries.Where(x => x.Details.ContainsKey(stage) && x.Details[stage].ContainsKey(Level.Hard) && x.Details[stage][Level.Hard].Item3 == hardTime).ToList();
-
-                stageWorldRecordEntries.Add(new StageWorldRecordItemData
-                {
-                    EasyColoredInitials = easyEntries.OrderBy(x => x.Details[stage][Level.Easy].Item4).Select(x => (ToInitials(x.PlayerName), x.PlayerColor)).ToList(),
-                    EasyTime = new TimeSpan(0, 0, easyTime),
-                    HardColoredInitials = hardEntries.OrderBy(x => x.Details[stage][Level.Hard].Item4).Select(x => (ToInitials(x.PlayerName), x.PlayerColor)).ToList(),
-                    HardTime = new TimeSpan(0, 0, hardTime),
-                    Image = $@"D:\Ma programmation\csharp\Projects\TheEliteUI\TheEliteUI\Resources\Stages\{(int)stage}.jpg",
-                    MediumColoredInitials = mediumEntries.OrderBy(x => x.Details[stage][Level.Medium].Item4).Select(x => (ToInitials(x.PlayerName), x.PlayerColor)).ToList(),
-                    MediumTime = new TimeSpan(0, 0, mediumTime),
-                    Name = stage.ToString(),
-                    Code = $"s{(int)stage}"
-                });
-                easySeconds += easyTime;
-                mediumSeconds += mediumTime;
-                hardSeconds += hardTime;
-            }
-
-            return View("Views/SimulatedRanking.cshtml", new SimulatedRankingViewData
-            {
-                CombinedTime = new TimeSpan(0, 0, easySeconds + mediumSeconds + hardSeconds),
-                EasyCombinedTime = new TimeSpan(0, 0, easySeconds),
-                MediumCombinedTime = new TimeSpan(0, 0, mediumSeconds),
-                HardCombinedTime = new TimeSpan(0, 0, hardSeconds),
+                CombinedTime = new TimeSpan(0, 0, secondsLevel.Values.Sum()),
+                EasyCombinedTime = new TimeSpan(0, 0, secondsLevel[Level.Easy]),
+                MediumCombinedTime = new TimeSpan(0, 0, secondsLevel[Level.Medium]),
+                HardCombinedTime = new TimeSpan(0, 0, secondsLevel[Level.Hard]),
                 PointsRankingEntries = pointsRankingEntries,
                 TimeRankingEntries = timeRankingEntries,
                 StageWorldRecordEntries = stageWorldRecordEntries
             });
-        }
-
-        private string ToInitials(string playerName)
-        {
-            var splitter = playerName.Split(' ');
-
-            if (splitter.Length < 2)
-                return splitter[0].Substring(0, 2).ToUpperInvariant();
-            else if (splitter.Length == 2)
-                return string.Concat(splitter[0].Substring(0, 1), splitter[1].Substring(0, 1)).ToUpperInvariant();
-            else
-                return string.Concat(splitter[0].Substring(0, 1), splitter[2].Substring(0, 1)).ToUpperInvariant();
         }
     }
 }
---
+++ TheEliteExplorerUi/Models/Converters.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TheEliteExplorerCommon;
+using TheEliteExplorerDomain.Enums;
+using TheEliteExplorerDomain.Models;
+
+namespace TheEliteExplorerUi.Models
+{
+    internal static class Converters
+    {
+        internal static PointsRankingItemData ToPointsRankingItemData(this RankingEntry entry)
+        {
+            return new PointsRankingItemData
+            {
+                EasyPoints = entry.LevelPoints[Level.Easy],
+                HardPoints = entry.LevelPoints[Level.Hard],
+                MediumPoints = entry.LevelPoints[Level.Medium],
+                PlayerColor = entry.PlayerColor,
+                PlayerName = entry.PlayerName,
+                Rank = entry.Rank,
+                TotalPoints = entry.Points
+            };
+        }
+
+        internal static TimeRankingItemData ToTimeRankingItemData(this RankingEntry entry, int rank)
+        {
+            return new TimeRankingItemData
+            {
+                EasyTime = new TimeSpan(0, 0, (int)entry.LevelCumuledTime[Level.Easy]),
+                HardTime = new TimeSpan(0, 0, (int)entry.LevelCumuledTime[Level.Hard]),
+                MediumTime = new TimeSpan(0, 0, (int)entry.LevelCumuledTime[Level.Medium]),
+                PlayerColor = entry.PlayerColor,
+                PlayerName = entry.PlayerName,
+                Rank = rank,
+                TotalTime = new TimeSpan(0, 0, (int)entry.CumuledTime)
+            };
+        }
+
+        internal static StageWorldRecordItemData ToStageWorldRecordItemData(this Stage stage,
+            List<RankingEntry> rankingEntries,
+            Dictionary<Level, int> secondsLevelCollector,
+            string stageImagePath)
+        {
+            var coloredInitialsLevel = new Dictionary<Level, List<(string, string)>>();
+            var secondsLevelStage = new Dictionary<Level, int>();
+            foreach (var level in SystemExtensions.Enumerate<Level>())
+            {
+                var bestTime = GetStageAndLevelBestTime(rankingEntries, stage, level);
+                secondsLevelStage.Add(level, bestTime);
+                coloredInitialsLevel.Add(level, GetPlayersRankedAtStageAndLevelTime(rankingEntries, stage, level, bestTime));
+                secondsLevelCollector[level] += bestTime;
+            }
+
+            return new StageWorldRecordItemData
+            {
+                EasyColoredInitials = coloredInitialsLevel[Level.Easy],
+                EasyTime = new TimeSpan(0, 0, secondsLevelStage[Level.Easy]),
+                MediumColoredInitials = coloredInitialsLevel[Level.Medium],
+                MediumTime = new TimeSpan(0, 0, secondsLevelStage[Level.Medium]),
+                HardColoredInitials = coloredInitialsLevel[Level.Hard],
+                HardTime = new TimeSpan(0, 0, secondsLevelStage[Level.Hard]),
+                Image = string.Format(stageImagePath, (int)stage),
+                Name = stage.ToString(),
+                Code = $"s{(int)stage}"
+            };
+        }
+
+        private static List<(string, string PlayerColor)> GetPlayersRankedAtStageAndLevelTime(List<RankingEntry> rankingEntries, Stage stage, Level level, int bestTime)
+        {
+            return rankingEntries
+                .Where(x => x.Details.ContainsKey(stage)
+                    && x.Details[stage].ContainsKey(level)
+                    && x.Details[stage][level].Item3 == bestTime)
+                .OrderBy(x => x.Details[stage][level].Item4)
+                .Select(x => (x.PlayerName.ToInitials(), x.PlayerColor))
+                .ToList();
+        }
+
+        private static int GetStageAndLevelBestTime(List<RankingEntry> rankingEntries, Stage stage, Level level)
+        {
+            return (int)rankingEntries
+                .Where(x => x.Details.ContainsKey(stage)
+                    && x.Details[stage].ContainsKey(level))
+                .OrderBy(x => x.Details[stage][level].Item3)
+                .First()
+                .Details[stage][level]
+                .Item3;
+        }
+
+        private static string ToInitials(this string playerName)
+        {
+            var parts = playerName
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(_ => _.Trim())
+                .ToArray();
+
+            var initials = parts.Length < 2
+                ? parts[0].Substring(0, 2)
+                : string.Concat(parts[0].Substring(0, 1), parts[parts.Length == 2 ? 1 : 2].Substring(0, 1));
+
+            return initials.ToUpperInvariant();
+        }
+    }
+}
