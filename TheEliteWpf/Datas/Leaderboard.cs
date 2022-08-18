f9b860af848ee3b2caf92782b011cd881a88c31b
Lpi <thelpi.v3@gmail.com>
2022-08-18 19:45:05 +0200

    data contract

---
+++ TheEliteWpf/Datas/Leaderboard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheEliteWpf.Datas
+{
+    public class Leaderboard
+    {
+        public IReadOnlyCollection<LeaderboardItem> Items { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+        public Stage Stage { get; set; }
+    }
+}
---
+++ TheEliteWpf/Datas/LeaderboardItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheEliteWpf.Datas
+{
+    public class LeaderboardItem
+    {
+        public Player Player { get; set; }
+        public int Points { get; set; }
+        public DateTime LatestTime { get; set; }
+        public int Rank { get; set; }
+    }
+}
